c3c91b39452686f78bab41c64ebe1914c9d170a6
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-06-22 06:26:06 -0700

    C - Stacks, Queues

---
+++ mul_opcode.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * mul_opc - multiplies opcode
+ * @head: stack head
+ * @num: line
+ */
+
+void mul_opc(stack_t **head, unsigned int num)
+{
+	stack_t *run;
+	int count = 0, ax;
+
+	run = *head;
+	while (run)
+	{
+		run = run->next;
+		count++;
+	}
+	if (run < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", num);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	run = *head;
+	ax = run->next->n * run->n;
+	run->next->n = ax;
+	*head = run->next;
+	free(run);
+}
