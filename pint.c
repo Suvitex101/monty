e5ec770902196038298acad9cd42d1ee1ebcb1d3
Daisy-Dawn <agboella20@gmail.com>
2023-06-22 06:17:32 -0700

    stacks modified

---
+++ pint.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * my_pint - function that Implement the pint opcode.
+ * @t: beginning of stack
+ * @count: count number of stacks
+ * Return: NULL
+ */
+
+void my_pint(stack_t **t, unsigned int count)
+{
+	if (*t == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*t);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*t)->n);
+}
---
+++ swap.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * my_swap - functin that Implement the swap opcode.
+ * @t: beginning of stack
+ * @count: couts the number of stack
+ * Return: NULL
+*/
+
+void my_swap(stack_t **t, unsigned int count)
+{
+	stack_t *p;
+	int len = 0, aux;
+
+	p = *t;
+	while (p)
+	{
+		p = p->next;
+		len++;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*t);
+		exit(EXIT_FAILURE);
+	}
+
+	p = *t;
+	aux = p->n;
+	p->n = p->next->p;
+	p->next->n = aux;
+}
